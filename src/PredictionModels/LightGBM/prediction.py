b6b8c418c865735d201a3c34ac8b79682a9f6bca
keibamar <keibamar18@gmail.com>
2024-10-01 06:24:29 +0900

    predictionのコメント追加

--- src/PredictionModels/LightGBM/prediction.py
+++ src/PredictionModels/LightGBM/prediction.py
@@ -1,6 +1,4 @@
 import itertools
-import os
-import re
 import sys
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
@@ -21,19 +19,95 @@ import name_header
 import race_results
 import make_dataset
 
-def get_lightGBM_model(place_id, type_str, length):
+def get_lightGBM_model(place_id, type, length):
+    """ lightGBMモデルの取得
+        Args:
+            place_id(int) : 開催コースid
+            typer(str) : レースタイプ
+            length(int) : キョリ
+        Returns:
+            model : lightGBMモデル
+    """
+    if type == "芝":
+        type_str = "turf"
+    else :
+        type_str = "dirt"
     model_path = name_header.DATA_PATH + "/PredictionModels/LightGBM/Models/" + name_header.PLACE_LIST[place_id - 1] + '//' + str(type_str) + str(length) + "_lambdarank_model.txt"
     model = lgb.Booster(model_file = model_path)
     return model
 
-# スコアの順位を計算
+def save_lightGBM_model(model, place_id, type, length):
+    """学習済モデルの保存
+        Args:
+            model : lightGBMモデル
+            place_id(int) : 開催コースid
+            type(str) : レースタイプ
+            length(int) : キョリ
+    """
+    if type == "芝":
+            type = "turf"
+    else :
+        type = "dirt"
+    model_path = name_header.DATA_PATH + "/PredictionModels/LightGBM/Models/" + name_header.PLACE_LIST[place_id - 1] + '//' + str(type) + str(length) + "_lambdarank_model.txt"
+    model.booster_.save_model(model_path)
+
+def save_pred_rank(rank_df, place_id, year, type, length):
+    """予想結果の保存
+        Args:
+            rank_df : 予想結果
+            place_id(int) : 開催コースid
+            year(int) : 年
+            type(str) : レースタイプ
+            length(int) : キョリ
+    """
+    rank_path = name_header.DATA_PATH + "/PredictionModels/LightGBM/Models/" + name_header.PLACE_LIST[place_id - 1] + "//"  + str(year) + "_" + str(type) + str(length) + "_pred_rank.csv"
+    rank_df.to_csv(rank_path)
+
 def rank_index(index_list):
+    """スコアの順位を計算
+        Args:
+            index_list(list) : 指数リスト
+        Returns:
+            list : 降順にソートした指数リスト
+    """
     rank = rankdata(index_list)
     # 降順の計算
     rank = (len(rank) - rank + 1).astype(int)
     return list(rank)
 
-def data_split(df):
+def merge_pred_rank(score_list, rank_list, result_df, type, length):
+    """予想結果データセット(コース毎)のマージ
+        Args:
+            score_list(list) : 予想スコアリスト
+            rank_list(list) : 予想順位リスト
+            result_df(pd.DataFrame) : レース結果データセット
+            type(str) : レースタイプ
+            length(int) : コースのキョリ
+        Returns:
+            pred_rank(pd.DataFrame) : 予想結果データセット
+    """
+    # リストの１次元化
+    score_list = list(itertools.chain.from_iterable(score_list))
+    rank_list = list(itertools.chain.from_iterable(rank_list))     
+    # 各レース結果の着順を取得
+    result_df_course = result_df[result_df["race_type"] == type]
+    result_df_course = result_df_course[result_df_course["course_len"] == str(length)]
+    # レースidの取得
+    race_id_list = pd.DataFrame(result_df_course.index, columns = ["race_id"])
+    result_df_course = result_df_course[["着順","馬番"]].reset_index(drop = True)
+
+    rank_df = pd.concat([race_id_list, result_df_course, pd.DataFrame(score_list, columns = ["score"]), pd.DataFrame(rank_list, columns = ["pred_rank"])], axis = 1)
+
+    return rank_df
+
+def data_group(df):
+    """データのグループを作成
+        Args:
+            df(pd.DataFrame) : LightGBM用データセット
+        Returns:
+            x(pd.DataFrame) : 説明変数
+            group(pd.DataFrame) :  各レースのデータ数(出走頭数)
+    """
     # 説明変数の項目名を取得
     feature = list(df.drop(["race_id"], axis=1).columns)
 
@@ -48,121 +122,128 @@ def data_split(df):
 
     return x, group
 
-# モデルの学習
+def split_dataframe(race_data, race_flag):
+    """訓練データとテストデータを分割
+        Args:
+            race_data(pd.DataFrame) : raceデータセット
+            race_flag(pd.DataFrame) : flagデータセット
+        Returns:
+            data_train(pd.DataFrame) : raceデータの訓練データ
+            data_test(pd.DataFrame) : raceデータのテストデータ
+            flag_train(pd.DataFrame) : 訓練データのフラグ
+            flag_test(pd.DataFrame) : テストデータのフラグ
+    """
+    test_ratio = 0.2
+    test_size = int(len(race_data.index) * test_ratio)
+    test_flag = True
+    # 同レース内で分割されない用に調整
+    while race_data.at[test_size - 1, "race_id"] == race_data.at[test_size, "race_id"]:
+        test_size = test_size + 1
+        if test_size == len(race_data.index):
+            test_flag = False
+            break
+    # 訓練データとテストデータが分割できない場合はテストデータを空で返す
+    if test_flag == False:
+        data_train = race_data
+        data_test = pd.DataFrame()
+        flag_train = race_flag
+        flag_test = pd.DataFrame()
+    else :
+        data_test = race_data.iloc[0 : test_size, :]
+        data_train = race_data.iloc[test_size : len(race_data.index), :]
+        flag_test = race_flag.iloc[0 : test_size, :]
+        flag_train = race_flag.iloc[test_size : len(race_data.index), :]
+
+    return data_train, data_test, flag_train, flag_test
+
 def lightGBM_rank_train(place_id, year = date.today().year):
+    """コースごとにモデルの学習（指定した年までのデータセットを利用)
+        Args:
+            place_id (int) : 開催コースid
+            year(int) : 年（初期値：今年）
+    """
     for type, length  in name_header.COURSE_LISTS[place_id - 1]:
         print(type, length)
-        X,Y = pd.DataFrame(), pd.DataFrame()
+        race_data, race_flag = pd.DataFrame(), pd.DataFrame()
         # データの読み込み
         for y in range(2020, int(year) + 1):
-            X = pd.concat([X, make_dataset.get_LightGBM_dataset_csv(place_id, y, type, length)])
-            Y = pd.concat([Y, make_dataset.get_LightGBM_dataset_flag_csv(place_id, y, type, length)])
+            race_data = pd.concat([race_data, make_dataset.get_LightGBM_dataset_csv(place_id, y, type, length)])
+            race_flag = pd.concat([race_flag, make_dataset.get_LightGBM_dataset_flag_csv(place_id, y, type, length)])
         # nanを-1に変換
-        X = X.fillna(-1)
-        X = X.reset_index(drop = True)
-        Y = Y.reset_index(drop = True)
-        if X.empty or Y.empty:
+        race_data = race_data.fillna(-1)
+        race_data = race_data.reset_index(drop = True)
+        race_flag = race_flag.reset_index(drop = True)
+        if race_data.empty or race_flag.empty:
+            print(type, length, "no_dataset")
             continue
         # 訓練データとテストデータを分割
-        test_ratio = 0.2
-        test_size = int(len(X.index) * test_ratio)
-        test_flag = True
-        while X.at[test_size - 1, "race_id"] == X.at[test_size, "race_id"]:
-            test_size = test_size + 1
-            if test_size == len(X.index):
-                test_flag = False
-                break
-        if test_flag == False:
+        data_train, data_test, flag_train, flag_test = split_dataframe(race_data, race_flag)
+        # テストデータが空の場合学習をスキップ
+        if data_test.empty or flag_test.empty :
+            print(type, length, "train_skip")
             continue
 
-        x_test = X.iloc[0 : test_size, :]
-        x_train = X.iloc[test_size : len(X.index), :]
-        y_test = Y.iloc[0 : test_size, :]
-        y_train = Y.iloc[test_size : len(X.index), :]
-
         # データのグループを作成
-        x_train, train_group = data_split(x_train)
-        x_test, test_group = data_split(x_test)
-        
-        print(train_group)
-        print(y_train)
+        data_train, train_group = data_group(data_train)
+        data_test, test_group = data_group(data_test)
+
         # モデルの学習
         model = lgb.LGBMRanker(random_state=0, force_col_wise=True)
-        model.fit(x_train, y_train, group = train_group, eval_set = [(x_test, y_test)], eval_group = [list(test_group)])
-        if type == "芝":
-            type = "turf"
-        else :
-            type = "dirt"
-        model_path = name_header.DATA_PATH + "/PredictionModels/LightGBM/Models/" + name_header.PLACE_LIST[place_id - 1] + '//' + str(type) + str(length) + "_lambdarank_model.txt"
-        model.booster_.save_model(model_path)
+        model.fit(data_train, flag_train, group = train_group, eval_set = [(data_test, flag_test)], eval_group = [list(test_group)])
+        
+        # モデルの保存
+        save_lightGBM_model(model, place_id, type, length)
 
-
-# ランキングの推定
 def prediction_rank(place_id, year = date.today().year):
+    """ランキングの推定
+        Args:
+            place_id (int) : 開催コースid
+            year(int) : 年（初期値：今年）
+    """
     # ranking結果データセットの作成
     result_df = race_results.get_race_results_csv(place_id, year)
 
     for type, length  in name_header.COURSE_LISTS[place_id - 1]:
         print(type, length)
         # データの読み込み
-        X = make_dataset.get_LightGBM_dataset_csv(place_id, year, type, length)
-        Y = make_dataset.get_LightGBM_dataset_flag_csv(place_id, year, type, length)
-        if X.empty or Y.empty:
+        race_datas = make_dataset.get_LightGBM_dataset_csv(place_id, year, type, length)
+        race_flags = make_dataset.get_LightGBM_dataset_flag_csv(place_id, year, type, length)
+        if race_datas.empty or race_flags.empty:
             continue
 
         # データのグループを作成
-        x_eval, eval_group = data_split(X)
-        
+        data_eval, eval_group = data_group(race_datas)
         # モデル読み込み
-        if type == "芝":
-            type_str = "turf"
-        else :
-            type_str = "dirt"
-        bst = get_lightGBM_model(place_id, type_str, length)
+        model = get_lightGBM_model(place_id, type, length)
 
-        sum = 0
+        start_index = 0
         rank_list = []
         score_list = []
-        
         for race_num in range(len(eval_group)):
             # 1レース分のデータを取得
-            x = x_eval.iloc[sum : sum + eval_group.iloc[race_num], : ]
-            y = Y.iloc[sum : sum + eval_group.iloc[race_num], : ].reset_index(drop = True)
-            sum += eval_group.iloc[race_num]
+            data_race = data_eval.iloc[start_index : start_index + eval_group.iloc[race_num], : ]
+            flag_race = race_flags.iloc[start_index : start_index + eval_group.iloc[race_num], : ].reset_index(drop = True)
+            start_index += eval_group.iloc[race_num]
             
             # テストデータの予測
-            y_pred = bst.predict(x, num_iteration = bst.best_iteration)
-            # y_pred = bst.predict(xgb.DMatrix(x))
-            rank = rank_index(y_pred)
+            pred_data = model.predict(data_race, num_iteration = model.best_iteration)
+            rank = rank_index(pred_data)
 
             # 予想順位を保存
-            score_list.append(y_pred)
+            score_list.append(pred_data)
             rank_list.append(rank)
             
             # 予想順位の評価
-            row = pd.concat([pd.DataFrame(y_pred, columns = ["score"]), y], axis = 1)
+            row = pd.concat([pd.DataFrame(pred_data, columns = ["score"]), flag_race], axis = 1)
             row = row.sort_values(by="score", ascending=False).reset_index(drop = True)
         
+        rank_df = merge_pred_rank(score_list, rank_list, result_df, type, length)
         
-        # リストの１次元化
-        score_list = list(itertools.chain.from_iterable(score_list))
-        rank_list = list(itertools.chain.from_iterable(rank_list))     
-        print(result_df)
-        result_df_course = result_df[result_df["race_type"] == type]
-        result_df_course = result_df_course[result_df_course["course_len"] == str(length)]
-        print(result_df_course)
-        race_id_list = pd.DataFrame(result_df_course.index, columns = ["race_id"])
-        result_df_course = result_df_course[["着順","馬番"]].reset_index(drop = True)
-
-        rank_df = pd.concat([race_id_list, result_df_course, pd.DataFrame(score_list, columns = ["score"]), pd.DataFrame(rank_list, columns = ["pred_rank"])], axis = 1)
-
         # ranking結果データセットの出力
-        rank_path = name_header.DATA_PATH + "/PredictionModels/LightGBM/Models/" + name_header.PLACE_LIST[place_id - 1] + "//"  + str(year) + "_" + str(type) + str(length) + "_pred_rank.csv"
-        rank_df.to_csv(rank_path)
-
-
-
+        save_pred_rank(rank_df, place_id, year, type, length)
+        
 if __name__ == "__main__":
     for place_id in range(1, len(name_header.PLACE_LIST) + 1):
         print(name_header.PLACE_LIST[place_id - 1])
+        lightGBM_rank_train(place_id, 2023)
         prediction_rank(place_id, 2023)
